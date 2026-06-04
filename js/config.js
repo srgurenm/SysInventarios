ff0fd215099f1e0cea647c7702d72ceb90a2ff42
srgurenm <jcsierrah77@gmail.com>
2026-06-04 14:35:39 -0500

    chore: upload production config

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/**
+ * config.js
+ * INSTRUCCIONES:
+ * 1. NO subas 'config.js' al control de versiones (git).
+ * 2. Añade 'config.js' a tu archivo .gitignore.
+ */
+window.GEMINI_API_KEY = "AIzaSyDecCHOKbuhFiCXNbI13IJ4njKz_SmJGOM";
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA93OGMhXmfSkIdnx3Eai8yFbuJiW0bKkc",
+  authDomain: "sysinventarios-beaa1.firebaseapp.com",
+  projectId: "sysinventarios-beaa1",
+  storageBucket: "sysinventarios-beaa1.firebasestorage.app",
+  messagingSenderId: "531940530426",
+  appId: "1:531940530426:web:dd1595196bf3ae10efd143",
+  measurementId: "G-1C05WMHNVK"
+};
