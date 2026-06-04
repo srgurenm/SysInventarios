90b005763398f4b07b6fa29aab5fd8b7a4ba1f3c
srgurenm <jcsierrah77@gmail.com>
2026-06-04 08:33:58 -0500

    chore: include js/config.js in repo for github pages

---
+++ js/config.js
@@ -0,0 +1,15 @@
+/**
+ * config.js
+ * INSTRUCCIONES:
+ * 1. NO subas 'config.js' al control de versiones (git).
+ * 2. Añade 'config.js' a tu archivo .gitignore.
+ */
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyA93OGMhXmfSkIdnx3Eai8yFbuJiW0bKkc",
+  authDomain:        "sysinventarios-beaa1.firebaseapp.com",
+  projectId:         "sysinventarios-beaa1",
+  storageBucket:     "sysinventarios-beaa1.firebasestorage.app",
+  messagingSenderId: "531940530426",
+  appId:             "1:531940530426:web:dd1595196bf3ae10efd143",
+  measurementId:     "G-1C05WMHNVK"
+};
